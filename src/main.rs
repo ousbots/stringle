1711ce671a8b476940e49d0407940d6d79048d51
tim <dev@sourbone.com>
2025-09-20 01:08:13 -0600

    refactor: move device initialization into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,15 @@
-use candle_core::Device;
 use std::fs;
 
+mod device;
 mod neural_net;
 mod options;
 
 fn main() {
     let options = options::parse_args();
-    let device = open_device(&options.device);
+    let device = device::open_device(&options.device);
     let data = parse_data(&options.data);
 
     neural_net::run(&data, &device, &options);
-}
-
-// Open the given device for data processing.
-fn open_device(device_name: &String) -> Device {
-    let mut device = Device::Cpu;
-    match device_name.trim().to_lowercase().as_str() {
-        "cpu" => {
-            println!("using the CPU for processing");
-            device = Device::Cpu;
-        }
-        "cuda" => {
-            println!("using cuda for processing");
-            device = Device::new_cuda(0).unwrap_or_else(|error| {
-                panic!("unable to open cuda device: {error:?}");
-            });
-        }
-        "metal" => {
-            println!("using metal for processing");
-            device = Device::new_metal(0).unwrap_or_else(|error| {
-                panic!("unable to open metal device: {error:?}");
-            });
-        }
-        _ => {
-            println!("invalid device name {device_name:?}, falling back to CPU for processing")
-        }
-    }
-
-    return device;
 }
 
 // Read the data into a list of strings using newlines as a separator.
